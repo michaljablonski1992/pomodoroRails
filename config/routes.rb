e8aa2695b5ac0ac6970fb630ed5a5ed411020830
Michał Jabłoński <macscrat11@MacBook-Pro-Micha-Jabonski.local>
2016-05-24 20:52:55 +0200

    api routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,14 @@ Rails.application.routes.draw do
 
   root "home#index"
 
+
+  namespace :api do
+    namespace :v1 do
+      resources :registrations, only: :create
+      resources :sessions, only: [:create, :destroy]
+    end
+  end
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
