d9e274b866f79b74a437cdd746841c4fd7a2c129
Michał Jabłoński <macscrat11@MacBook-Pro-Micha-Jabonski.local>
2016-05-24 23:05:56 +0200

    user model fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   acts_as_token_authenticatable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable,
+  :trackable, :validatable, :confirmable
 
 end
