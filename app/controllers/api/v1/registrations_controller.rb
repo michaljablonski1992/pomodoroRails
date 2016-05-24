c29a2071393e52a8791f67d8e69718d4410e4170
Michał Jabłoński <macscrat11@MacBook-Pro-Micha-Jabonski.local>
2016-05-24 21:56:13 +0200

    meh

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
   skip_before_filter :verify_authenticity_token
 
   def create
-    build_resource(params[:user])
+    build_resource(sign_up_params)
     resource.skip_confirmation!
     if resource.save
       sign_in(resource, :store => false)
