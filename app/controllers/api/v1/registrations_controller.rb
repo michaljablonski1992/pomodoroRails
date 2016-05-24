1f3e757b0488c4cf736185ccfc0cbe6857c7ca8f
Michał Jabłoński <macscrat11@MacBook-Pro-Micha-Jabonski.local>
2016-05-24 22:05:26 +0200

    no message

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -3,12 +3,12 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
   skip_before_filter :verify_authenticity_token
 
   def create
-    build_resource(params[:registration])
+    build_resource(sign_up_params)
     resource.skip_confirmation!
     if resource.save
       sign_in(resource, :store => false)
       render :status => 200,
-           :json => { :success => true,
+            :json => { :success => true,
                       :info => "Registered",
                       :data => { :user => resource,
                                  :auth_token => current_user.authentication_token } }
