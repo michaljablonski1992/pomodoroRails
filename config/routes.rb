e8166095abc2b07a05366a174e4fc784e6829812
Michał Jabłoński <macscrat11@MacBook-Pro-Micha-Jabonski.local>
2016-05-24 20:57:12 +0200

    routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
 
   root "home#index"
 
+  devise_for :users
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
+
+  # You can have the root of your site routed with "root"
+  # root 'welcome#index'
 
   namespace :api do
     namespace :v1 do
@@ -9,13 +15,6 @@ Rails.application.routes.draw do
       resources :sessions, only: [:create, :destroy]
     end
   end
-
-  devise_for :users
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
