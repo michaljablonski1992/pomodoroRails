0c42f8583c6f99dbbd657d952b8ffc2f63009fc9
Michał Jabłoński <macscrat11@MacBook-Pro-Micha-Jabonski.local>
2016-05-24 22:14:50 +0200

    plyz work

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,9 +1,11 @@
 class Api::V1::RegistrationsController < Devise::RegistrationsController
 
   skip_before_filter :verify_authenticity_token
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
 
   def create
-    super 
+    build_resource(sign_up_params)
     resource.skip_confirmation!
     if resource.save
       sign_in(resource, :store => false)
@@ -19,4 +21,12 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
                         :data => {} }
     end
   end
+
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email])
+  end
+
 end
