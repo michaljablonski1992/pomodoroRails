5fd08ad0b6936300d9cf5024c2bac7c382fc6d1e
Michał Jabłoński <macscrat11@MacBook-Pro-Micha-Jabonski.local>
2016-05-24 21:12:30 +0200

    routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      devise_for :users, controllers: { registrations: "registrations", sessions:"sessions" }
+      resources :registrations, only: :create
+      resources :sessions, only: [:create, :destroy]
     end
   end
 
