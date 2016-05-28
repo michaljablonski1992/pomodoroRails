2232b75f58632a610582360749f10a33a3e344a0
Michał Jabłoński <macscrat11@MBPMichJabonski.home>
2016-05-28 17:10:17 +0200

    musi tak byc

--- app/controllers/api/v1/pomodoros_made_controller.rb
+++ app/controllers/api/v1/pomodoros_made_controller.rb
@@ -1,7 +1,5 @@
 class Api::V1::PomodorosMadeController < ApplicationController
 
-  skip_before_filter :verify_authenticity_token
-
   def show
     pomodoros_count = current_user.pomodoros_made.count
     render :status => 200,
@@ -10,8 +8,9 @@ class Api::V1::PomodorosMadeController < ApplicationController
   end
 
   def update
+    auth_token = params[:user][:auth_token]
     pomodoros_count = params[:pomodoros_made][:count]
-    pomodoros_made = current_user.pomodoros_made
+    pomodoros_made = User.where(authentication_token: auth_token).first.pomodoros_made
     pomodoros_made.count = pomodoros_count
     pomodoros_made.save
     render :status => 200,
