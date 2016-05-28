bf67936965b0fb29590c03e2f8af129397f93e6c
Michał Jabłoński <macscrat11@MBPMichJabonski.home>
2016-05-28 17:39:36 +0200

    raise raise!

--- app/controllers/api/v1/pomodoros_made_controller.rb
+++ app/controllers/api/v1/pomodoros_made_controller.rb
@@ -12,9 +12,8 @@ class Api::V1::PomodorosMadeController < ApplicationController
   def update
     auth_token = params[:user][:auth_token]
     pomodoros_count = params[:pomodoros_made][:count]
-    err = "token :#{auth_token}, count:#{pomodoros_count}"
-    raise err.inspect
     pomodoros_made = User.where(authentication_token: auth_token).first.pomodoros_made
+    raise pomodoros_made.inspect
     pomodoros_made.count = pomodoros_count
     pomodoros_made.save
     render :status => 200,
