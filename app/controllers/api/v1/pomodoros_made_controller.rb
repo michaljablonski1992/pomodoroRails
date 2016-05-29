5bea92fdfd9a61be97617a512475278841614e8f
Michał Jabłoński <macscrat11@MBPMichJabonski.home>
2016-05-29 14:54:48 +0200

    pomodoros made controller fix

--- app/controllers/api/v1/pomodoros_made_controller.rb
+++ app/controllers/api/v1/pomodoros_made_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::PomodorosMadeController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def show
-    auth_token = params[:user][:auth_token]
+    auth_token = params[:auth_token]
     pomodoros_made_count = User.where(authentication_token: auth_token.to_s).first.pomodoros_made.count
     render :status => 200,
            :json => { :success => true,
